f1838255548b9b1313d003861dcc0eaea038a3f3
Saswat Samal <saswat1091samal@gmail.com>
2026-02-25 16:13:21 +0530

    add: interpreter and expression calculation
    - /src/jlox/Interpreter.java - added interepreter and expression
      calculation functionality for different types of expressions
    - /src/jlox/Jlox.java - added the interpreter function call
    - /src/jlox/RuntimeError.java - added the runtime error function for
      catching runtime errors during interpretation
    
    Signed-off-by: Saswat Samal <saswat1091samal@gmail.com>

--- src/jlox/Jlox.java
+++ src/jlox/Jlox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Jlox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -37,7 +40,10 @@ public class Jlox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code
         if(hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     private static void run(String source) {
@@ -46,16 +52,17 @@ public class Jlox {
         Parser parser =new Parser(tokens);
         Expr expression = parser.parse();
 
+        // Stop if there was a syntax error.
         if(hadError) return;
 
+        interpreter.interpret(expression);
         System.out.println(new AstPrinter().print(expression));
 
-        /*
-        *for(Token token: tokens) {
-        *    System.out.println(token);
-        *}
-        *
-        **/
+
+        for(Token token: tokens) {
+            System.out.println(token);
+        }
+
     }
 
     static void error(int line, String message) {
@@ -70,6 +77,11 @@ public class Jlox {
         }
     }
 
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
 
     private static void report(int line, String where, String message) {
         System.err.println(
