36767b94f99fbff2741849899bf0133df89e0a85
Saswat Samal <saswat1091samal@gmail.com>
2026-02-26 11:02:07 +0530

    add Stmt.java

--- src/tool/GeneratAst.java
+++ src/tool/GeneratAst.java
@@ -18,6 +18,11 @@ public class GeneratAst {
                 "Literal  : Object value",
                 "Unary    : Token operator, Expr right"
         ));
+
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
