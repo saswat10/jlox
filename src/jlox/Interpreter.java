f74eaf31ec0faa7a0e65e9a7601f268b59b828ed
Saswat Samal <saswat1091samal@gmail.com>
2026-02-26 13:53:41 +0530

    add: Block statments, print and variable implementation
    - /src/jlox/AstPrinter.java - add function implementation of Assignment
      Expression
    - /src/jlox/Environment.java - created new file, added environment
      functionality, which helps in distinguishing local and global scope of
      variables.
    - /src/jlox/Expr.java - Generated new visitor functions for the following
      classes - Assignment.
    - /src/jlox/Interpreter.java - add code for evaluation of expression within
      blocks and statements along with proper error handling.
    - /src/jlox/Stmt.java - Generated new visitor functions for Block class.
    - /src/tool/GenerateAst.java - add code for Block and Assign visitor
      class generation.
    
    Signed-off-by: Saswat Samal <saswat1091samal@gmail.com>

--- src/jlox/Interpreter.java
+++ src/jlox/Interpreter.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements) {
         try {
             for(Stmt statement: statements) {
@@ -20,7 +22,6 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     private void execute(Stmt stmt) {
         stmt.accept(this);
     }
-
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
@@ -142,4 +143,48 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         System.out.println(stringify(value));
         return null;
     }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if(stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
+    void executeBlock(List<Stmt> statements, Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+
+            for(Stmt statement: statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+
+
 }
