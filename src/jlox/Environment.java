96535170e540ae95ae4d1e825793ade974e1de19
Saswat Samal <saswat1091samal@gmail.com>
2026-03-03 23:16:45 +0530

    Resolving and binding fix
    Signed-off-by: Saswat Samal <saswat1091samal@gmail.com>

--- src/jlox/Environment.java
+++ src/jlox/Environment.java
@@ -15,6 +15,14 @@ class Environment {
         this.enclosing = enclosing;
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance ; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
     Object get(Token name){
         if(values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -43,5 +51,13 @@ class Environment {
 
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    public void assignAt(Integer distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 }
